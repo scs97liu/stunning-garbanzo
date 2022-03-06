ed3f28e942ffdb27110ba2aa3df09a0dbc344c60
scs97liu <shane1997liu@gmail.com>
2022-03-06 14:12:08 -0800

    Reorganized cosmos connection and routes

--- recipe-manager-backend/src/recipes/recipes.controller.ts
+++ recipe-manager-backend/src/recipes/recipes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Delete, Param, Body  } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body, Put  } from '@nestjs/common';
 import { RecipesService } from './recipes.service';
 import { Recipe } from './recipes.model'
 
@@ -11,26 +11,31 @@ export class RecipesController {
     async findAll(): Promise<Recipe[]> {
         return this.recipesService.findAll();
     }
+}
 
+@Controller('recipe')
+export class RecipeController {
+
+    constructor(private recipesService: RecipesService) {}
+    
     @Post() // /recipes
     async create(@Body() recipe: Recipe) {
         this.recipesService.create(recipe);
     }
-
     
     @Get(':id')
     findOne(@Param('id') id: string) {
       return this.recipesService.findById(id);
     }
 
-    @Patch(':id')
-    updateOne(@Param('id') recipe: Recipe) {
-      return this.recipesService.upsert(recipe);
-    }
-
-
     @Delete(':id')
     deleteOne(@Param('id') id: string) {
       return this.recipesService.remove(id, id);
     }
+
+    //TODO Update
+    // @Put(':id')
+    // updateOne(@Param('id') id: string, @Body() recipe: Recipe) {
+    //   return this.recipesService.update(id, recipe);
+    // }
 }
--- recipe-manager-backend/src/recipes/recipes.module.ts
+++ recipe-manager-backend/src/recipes/recipes.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AzureCosmosDbModule } from '@nestjs/azure-database';
-import { RecipesController } from './recipes.controller';
+import { RecipeController, RecipesController } from './recipes.controller';
 import { Recipe } from './recipes.model';
 import { RecipesService } from './recipes.service';
 
 @Module({
     imports: [
-        AzureCosmosDbModule.forRoot({
-            //TODO- FIX ENV Variable retrieval
-            dbName: process.env.AZURE_COSMOS_DB_NAME,
-            endpoint: process.env.AZURE_COSMOS_DB_ENDPOINT,
-            key: process.env.AZURE_COSMOS_DB_KEY,
-          }),
         AzureCosmosDbModule.forFeature([{ dto: Recipe }])
     ],
     providers: [RecipesService],
-    controllers: [RecipesController ],
+    controllers: [RecipesController, RecipeController ],
 })
 export class RecipesModule {}
